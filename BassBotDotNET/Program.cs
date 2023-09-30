01a93f68227faae830abf1321e84b4130cbc1c5c
SecretBetta <secretbetta@gmail.com>
2023-09-30 02:12:18 -0700

    added comment and TODO

--- BassBotDotNET/Program.cs
+++ BassBotDotNET/Program.cs
@@ -13,14 +13,9 @@ public class Program
 
         _client.Log += Log;
 
-        //  You can assign your bot token to a string, and pass that in to connect.
-        //  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
-        //var token = "token";
-
-        // Some alternative options would be to keep your token in an Environment Variable or a standalone file.
-         //var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
-         var token = File.ReadAllText("token.txt");
-        // var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
+        // Reading token from token.txt for testing.
+        // TODO set up secret management for production.
+        var token = File.ReadAllText("token.txt");
 
         await _client.LoginAsync(TokenType.Bot, token);
         await _client.StartAsync();
