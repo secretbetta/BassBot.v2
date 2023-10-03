e751db04e446055a17c743a947b3ca28e7c41dd0
Andrew Nguyen <secretbetta@gmail.com>
2023-10-02 20:18:16 -0700

    testing slash command because other command is higher privilege

--- BassBotDotNET/Program.cs
+++ BassBotDotNET/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Discord;
+using Discord.Net;
 using Discord.WebSocket;
+using Newtonsoft.Json;
 
 public class Program
 {
@@ -12,6 +14,7 @@ public class Program
         _client = new DiscordSocketClient();
 
         _client.Log += Log;
+        _client.Ready += Client_Ready;
 
         // Reading token from token.txt for testing.
         // TODO set up secret management for production.
@@ -28,5 +31,46 @@ public class Program
     {
         Console.WriteLine(msg.ToString());
         return Task.CompletedTask;
+    }
+
+    public async Task Client_Ready()
+    {
+        ulong guildId = 738870706191728772; // Testing Server
+
+        // Let's build a guild command! We're going to need a guild so lets just put that in a variable.
+        var guild = _client.GetGuild(guildId);
+
+        // Next, lets create our slash command builder. This is like the embed builder but for slash commands.
+        var guildCommand = new SlashCommandBuilder();
+
+        // Note: Names have to be all lowercase and match the regular expression ^[\w-]{3,32}$
+        guildCommand.WithName("first-command");
+
+        // Descriptions can have a max length of 100.
+        guildCommand.WithDescription("This is my first guild slash command!");
+
+        // Let's do our global command
+        var globalCommand = new SlashCommandBuilder();
+        globalCommand.WithName("first-global-command");
+        globalCommand.WithDescription("This is my first global slash command");
+
+        try
+        {
+            // Now that we have our builder, we can call the CreateApplicationCommandAsync method to make our slash command.
+            await guild.CreateApplicationCommandAsync(guildCommand.Build());
+
+            // With global commands we don't need the guild.
+            await _client.CreateGlobalApplicationCommandAsync(globalCommand.Build());
+            // Using the ready event is a simple implementation for the sake of the example. Suitable for testing and development.
+            // For a production bot, it is recommended to only run the CreateGlobalApplicationCommandAsync() once for each command.
+        }
+        catch (HttpException exception)
+        {
+            // If our command was invalid, we should catch an ApplicationCommandException. This exception contains the path of the error as well as the error message. You can serialize the Error field in the exception to get a visual of where your error is.
+            var json = JsonConvert.SerializeObject(exception.Errors, Formatting.Indented);
+
+            // You can send this error somewhere or just print it to the console, for this example we're just going to print it.
+            Console.WriteLine(json);
+        }
     }
 }
---
+++ BassBotDotNET/SlashCommandModules/TestCommand.cs
@@ -0,0 +1,10 @@
+﻿using Discord.Net;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BassBotDotNET.SlashCommandModules;
+
