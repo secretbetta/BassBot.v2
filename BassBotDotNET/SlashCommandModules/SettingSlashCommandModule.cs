ba7f2d8b8977ac671cd68be7459b5cd8cd9a6b7a
Justine Giaky Huynh <jgh75@georgetown.edu>
2023-10-05 21:21:29 -0700

    Tried adding method SendRichEmbedAsynch()

--- BassBotDotNET/SlashCommandModules/SettingSlashCommandModule.cs
+++ BassBotDotNET/SlashCommandModules/SettingSlashCommandModule.cs
@@ -118,4 +118,29 @@ public class SettingSlashCommandModule
                 break;
         }
     }
+    
+    [Command("embed")]
+    public async Task SendRichEmbedAsync()
+    {
+        var embed = new EmbedBuilder
+            {
+                // Embed property can be set within object initializer
+                Title = "Hello world!",
+                Description = "I am a description set by initializer."
+            };
+            // Or with methods
+        embed.AddField("Field title",
+            "Field value. I also support [hyperlink markdown](https://example.com)!")
+            .WithAuthor(Context.Client.CurrentUser)
+            .WithFooter(footer => footer.Text = "I am a footer.")
+            .WithColor(Color.Blue)
+            .WithTitle("I overwrote \"Hello world!\"")
+            .WithDescription("I am a description.")
+            .WithUrl("https://example.com")
+            .WithCurrentTimestamp();
+
+        //Your embed needs to be built before it is able to be sent
+        await ReplyAsync(embed: embed.Build());
+    }
+    
 }
